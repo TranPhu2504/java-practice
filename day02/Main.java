e587d6a57dd8b4b76a08b5b44bfdd30f1bf0faf7
Tranphu2504 <51800468@student.tdtu.edu.vn>
2025-07-22 11:43:29 +0700

    UD

--- day02/Main.java
+++ day02/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Student s = new Student("Nam", 21, "SE12345");
+        Student s = new Student(" ",0, "SE12345");
+        s.setName("Phu");
+        s.setAge(25);
         s.showInfo();
     }
 }
